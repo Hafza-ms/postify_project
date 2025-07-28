fdf8042b5f1ba4023b27bb4b477f86ffed4e8559
Hafza-ms <hafzams@gmail.com>
2025-07-28 14:35:40 +0530

    path correction

--- src/components/RoutingPages/Bookmarked.jsx
+++ src/components/RoutingPages/Bookmarked.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Blog from "../Data/Blog";
-import BlogCard from "../components/BlogCard";
+import BlogCard from "../BlogCard";
 
 function Bookmarked() {
   const [bookmarkedBlogs, setBookmarkedBlogs] = useState([]);
